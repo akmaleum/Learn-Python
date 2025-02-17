c579b411f1ef374025d5a53e7cabeb861940ed70
akmal <m_akmal_hisyam@soc.uum.edu.my>
2025-02-17 14:59:28 +0800

    initial commit

---
+++ arithmetic.py
@@ -0,0 +1,17 @@
+friends = 5
+
+# addition "+"
+friends = friends + 1
+
+# subtraction "-"
+friends = friends - 1
+
+# multiplication "*"
+friends = friends * 2
+
+# division "/"
+friends = friends / 2
+
+
+
+print(friends)
---
+++ variables.py
@@ -0,0 +1,16 @@
+# String - a series of characters
+full_name = "Akmal Hisyam"
+
+# integer - a set of whole numbers
+age = 23
+
+# float - numbers with decimals
+gpa = 3.08
+
+# boolean - true or false
+is_student = True
+
+print(f"Hello {full_name}")
+print(f"You are {age} years old")
+print(f"Your gpa is {gpa}")
+print(f"Are you currently a student? : {is_student}")
